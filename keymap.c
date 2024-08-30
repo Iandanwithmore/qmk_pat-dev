ef111312fef5308970a4dd78edb8489ac26b152b
Patrick Fuentes Carpio <33789701+pat-devcorp@users.noreply.github.com>
2024-08-30 16:19:30 -0400

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -11,32 +11,25 @@
 /* Layer keymaps */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,           KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-    KC_LSFT, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,           KC_H,     KC_J,    KC_K,    KC_L,    KC_MINUS, KC_QUOT,
-    KC_LCTL, KC_LALT, KC_Z,    KC_X,     KC_C,     KC_V,           KC_B,     KC_N,    KC_M,    KC_EQL,  KC_COLN,  KC_ESC,
-                               KC_LGUI,  KC_CAPS,  KC_RAISE,       KC_LOWER, KC_COMM, KC_DOT
+    KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,     KC_BSPC,
+    KC_LSFT, KC_A, KC_S, KC_D,    KC_F,     KC_G,      KC_H,     KC_J,     KC_K,    KC_L,    KC_QUOT,  KC_MINUS,
+    KC_LCTL, KC_Z, KC_X, KC_C,    KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SCLN,  KC_ESC,
+                         KC_LGUI, KC_LOWER, KC_SPC,    KC_ENT,   KC_RAISE, KC_RALT
     ),
 
     [_LOWER] = LAYOUT(
-    KC_CALC, KC_LEFT, KC_DOWN, KC_UP,    KC_RIGHT, KC_DEL,         KC_LT,    KC_7,    KC_8,    KC_9,    KC_CIRC,  KC_LPRN,
-    KC_LSFT, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,   XXXXXXX,        KC_GT,    KC_4,    KC_5,    KC_6,    KC_PPLS,  KC_PAST,
-    KC_LCTL, KC_SCLN, KC_UNDS, KC_DQUO,  KC_SLSH,  KC_PERC,        KC_0,     KC_1,    KC_2,    KC_3,    KC_MINS,  KC_SLSH,
-                               XXXXXXX,  XXXXXXX,  KC_RAISE,       KC_ENT,   XXXXXXX, KC_PDOT
+    KC_TAB,  KC_0,    KC_1,   KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_BSPC,
+    KC_LSFT, RGB_MOD, RGB_HUI RGB_VAI, KC_BRIU, XXXXXXX, KC_VOLU,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX, XXXXXXX,
+    KC_LCTL, RGB_TOG, RGB_HUD RGB_VAD, KC_BRID, XXXXXXX, KC_VOLD,    KC_HOME, KC_END,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,
+                                       KC_LGUI, XXXXXXX, KC_SPC      KC_ENT,  XXXXXXX, KC_RALT
     ),
 
     [_RAISE] = LAYOUT(
-    XXXXXXX, KC_AT,   KC_DLR,   KC_TILD, KC_HASH,  KC_TILD,       KC_DEL,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_BSPC,
-    KC_LSFT, KC_PIPE, KC_QUES,  KC_GRV,  KC_SLSH,  KC_BSLS,       XXXXXXX,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_RSFT,
-    KC_LCTL, XXXXXXX, XXXXXXX,  KC_MPRV, KC_MPLY,  KC_MNXT,       KC_LPRN,   KC_RPRN, KC_LBRC, KC_RBRC, KC_LCBR,  KC_RCBR,
-                                XXXXXXX, XXXXXXX,  KC_SPC,        KC_LOWER,  XXXXXXX, XXXXXXX
-    ),
-
-    [_ADJUST] = LAYOUT(
-    RGB_TOG, RGB_MOD, RGB_RMOD, XXXXXXX, XXXXXXX,  XXXXXXX,       KC_FIND,   KC_F5,   KC_F12,  KC_CLR,  KC_PSCR,  XXXXXXX,
-    XXXXXXX, RGB_HUI, RGB_SAI,  RGB_VAI, KC_VOLD,  KC_VOLU,       KC_SLEP,   KC_WSCH, KC_WREF, KC_WBAK, KC_WFWD,  XXXXXXX,
-    XXXXXXX, RGB_HUD, RGB_SAD,  RGB_VAD, KC_BRID,  KC_BRIU,       KC_MSTP,   KC_MPLY, KC_MRWD, KC_MFFD, XXXXXXX,  XXXXXXX,
-                                XXXXXXX, XXXXXXX,  XXXXXXX,       XXXXXXX,   XXXXXXX, XXXXXXX
+    KC_TAB,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC,    KC_CIRC, KC_AMPR, KC_LPRN, KC_RPRN, KC_PIPE, KC_BSPC,
+    KC_LSFT, KC_EQUAL, KC_QUES, XXXXXXX, XXXXXXX, XXXXXXX,    KC_MINS, KC_PAST, KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,
+    KC_LCTL, KC_CAPS,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_UNDS, KC_PPLS, KC_LBRC, KC_RBRC, KC_SLSH, KC_TILD,
+                                KC_LGUI, XXXXXXX,  KC_SPC,    KC_ENT,  XXXXXXX, KC_RALT
     )
 };
 
-layer_state_t layer_state_set_user(layer_state_t state) { return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST); }
+layer_state_t layer_state_set_user(layer_state_t state) { return update_tri_layer_state(state, _LOWER, _RAISE); }
